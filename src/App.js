5d21200ef810f80aecb867c760c6465b88a5c3d3
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-12-09 00:36:18 +0530

    Adding the rapid api and host and key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, Routes } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
 import {
@@ -20,7 +20,7 @@ const App = () => (
     <div className="main">
       <Layout>
         <div className="routes">
-          <Switch>
+          <Routes>
             <Route exact path="/">
               <Homepage />
             </Route>
@@ -36,7 +36,7 @@ const App = () => (
             <Route exact path="/news">
               <News />
             </Route>
-          </Switch>
+          </Routes>
         </div>
       </Layout>
       <div className="footer">
